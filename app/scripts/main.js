86db10ce18124c1b39e953a1ccdad2e260ef79e8
Daiwei Lu <daiwei@spanishdict.com>
2014-08-21 22:50:36 -0400

    Generate Sudoku
    1. Build game board
    2. Generate random puzzle
    3. Fill game board

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 /* global _: false */
 
-(function(window, _, undefined) {
+(function(window, _, $, undefined) {
 'use strict';
 
 var FULL_NUMBERS = [1,2,3,4,5,6,7,8,9];
@@ -56,7 +56,7 @@ function nextCell(x, y, numbers, matrix) {
   return true;
 }
 
-window.generateSudokuMatrix = function() {
+var generateSudokuMatrix = window.generateSudokuMatrix = function() {
   var matrix = new Array(9);
   for (var i = 0; i < 9; i++) {
     matrix[i] = new Array(9);
@@ -67,4 +67,49 @@ window.generateSudokuMatrix = function() {
   return matrix;
 };
 
-})(window, _);
+var getRandomCoords = window.getRandomCoords = function () {
+  var emptyNum = _.random(20, 40);
+  var coords   = [];
+  for (var i = 0; i < emptyNum; i++) {
+    var invalid = true;
+    while (invalid) {
+      var x = _.random(0, 8);
+      var y = _.random(0, 8);
+      if (!_.find(coords, [x, y])) {
+        coords.push([x, y]);
+        invalid = false;
+      }
+    }
+  }
+  return coords;
+};
+
+var generateSudokuPuzzel = window.generateSudokuPuzzel = function() {
+  var matrix = generateSudokuMatrix();
+  var coords = getRandomCoords();
+  for (var i = 0; i < coords.length; i++) {
+    var x = coords[i][0];
+    var y = coords[i][1];
+    matrix[y][x] = null;
+  }
+  return matrix;
+};
+
+var fillBoard = window.fillBoard = function() {
+  var matrix = generateSudokuPuzzel();
+  for (var x = 0; x < 9; x++) {
+    for (var y = 0; y < 9; y++) {
+      var n = matrix[y][x];
+      var $number = $('.number.row-'+y+'.column-'+x);
+      if (n != null) {
+        $number.html(n);
+      } else {
+        $number.attr('class', $number.attr('class')+' empty');
+      }
+    }
+  }
+};
+
+fillBoard();
+
+})(window, _, jQuery);
